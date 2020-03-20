3752a6e59ca6b03e54a69d2fe9d6239c6ae7bc91
Satya Wikananda <satyawikananda456@gmail.com>
2020-03-20 23:16:27 +0800

    :fire: Remove some item in navbar

--- src/components/master/Navbar.vue
+++ src/components/master/Navbar.vue
@@ -1,39 +1,9 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center">
-      <h5 class="title font-weight-regular ml-5 ">VuCovid | Covid info</h5>
-    </div>
-
-    <v-spacer></v-spacer>
-
-    <template>
-      <v-toolbar-items class="mr-5">
-        <v-btn v-for="item in menu" :key="item.key" text large>{{
-          item.title
-        }}</v-btn>
-      </v-toolbar-items>
-    </template>
-  </v-app-bar>
+  <header>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h5 class="title font-weight-regular ml-5 ">VuCovid | Covid info</h5>
+      </div>
+    </v-app-bar>
+  </header>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      menu: [
-        {
-          title: "Map Covid",
-          link: "/"
-        },
-        {
-          title: "Graph Image",
-          link: "/"
-        },
-        {
-          title: "Data Global",
-          link: "/"
-        }
-      ]
-    };
-  }
-};
-</script>
